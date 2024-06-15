eac6d58069175befac07af32f728389addd3c2da
Jorge Soriano <bowloforanges@Jorges-MacBook-Air.local>
2024-06-15 16:31:29 -0600

    Added UpdateUserEndpoint.

---
+++ Microservices/UserManagement/src/InterfaceAdapters/Controllers/UpdateUserController.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Controllers;
+
+[ApiController]
+[Route("api/v1")]
+public class UpdateUserController : ControllerBase
+{
+    private readonly ILogger<GetUserController> _logger;
+
+    public UpdateUserController(ILogger<GetUserController> logger) => (_logger) = (logger);
+
+    [HttpPut("users/{userId}")]
+    public async Task<IActionResult> GetAllUsers(Guid userId)
+    {
+        await Task.Delay(1);
+
+        _logger.LogInformation($"{GetType().Name} - Updated user with id {userId}");
+
+        GenericHttpResponse response = new GenericHttpResponse();
+        return Ok(response);
+    }
+}
